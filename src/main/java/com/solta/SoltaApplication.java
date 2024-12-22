5ae8e45ec3e99287725bb80b94f89107810c9c3b
최재익 <shameless8@naver.ocm>
2024-12-22 19:52:06 +0900

    init project

---
+++ src/main/java/com/solta/SoltaApplication.java
@@ -0,0 +1,13 @@
+package com.solta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoltaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solta/SoltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
