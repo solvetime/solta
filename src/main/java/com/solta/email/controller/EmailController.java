c79b7a47575c02e2111f1ebca2b68f54edce6b77
GiSeok Kang <ime.rokz@gmail.com>
2024-12-27 19:16:50 +0900

    feat: 이메일 중복 검사 로직 추가

--- src/main/java/com/solta/email/controller/EmailController.java
+++ src/main/java/com/solta/email/controller/EmailController.java
@@ -5,7 +5,11 @@ import com.solta.email.dto.EmailDTO;
 import com.solta.email.dto.VerificationEmailDTO;
 import com.solta.email.service.MailService;
 import com.solta.global.util.RedisUtil;
+import com.solta.member.domain.Member;
+import com.solta.member.repository.MemberRepository;
+import com.solta.member.service.MemberService;
 import jakarta.validation.Valid;
+import java.util.NoSuchElementException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +23,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
 
     private final MailService mailService;
+    private final MemberRepository memberRepository;
+
+    @PostMapping("/exists")
+    public ResponseEntity<EmailDTO> existsEmail(@RequestBody @Valid EmailDTO email) {
+        try {
+            Member byEmail = memberRepository.findByEmail(email.email());
+            return ResponseEntity.ok(email);
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
 
     @PostMapping("/send")
     public ResponseEntity<Void> sendEmail(@RequestBody @Valid EmailDTO email) {
