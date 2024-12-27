526d96eac6dee51dc20f0f6a399e1e47692fdbdc
GiSeok Kang <ime.rokz@gmail.com>
2024-12-27 20:03:36 +0900

    feat: JWT 엑세스 토큰 생성 로직 추가

---
+++ src/main/java/com/solta/global/token/JwtTokenManager.java
@@ -0,0 +1,41 @@
+package com.solta.global.token;
+
+import com.solta.auth.dto.AuthInfo;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTokenManager {
+
+    private final Key signingKey;
+    private final long validityInMilliseconds;
+    private final long refreshTokenValidityInMilliseconds;
+
+    public JwtTokenManager(
+            @Value("${custom.jwt.secret-key}") String secretKey,
+            @Value("${custom.jwt.expire.access}") long validityInMilliseconds,
+            @Value("${custom.jwt.expire.refresh}") long refreshTokenValidityInMilliseconds) {
+
+        this.signingKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+        this.validityInMilliseconds = validityInMilliseconds;
+        this.refreshTokenValidityInMilliseconds = refreshTokenValidityInMilliseconds;
+    }
+
+    public String createAccessToken(AuthInfo authInfo) {
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .claim("email", authInfo.email())
+                .claim("name", authInfo.name())
+                .issuedAt(now)
+                .expiration(validity)
+                .signWith(signingKey)
+                .compact();
+    }
+}
