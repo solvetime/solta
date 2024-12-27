c839c0df32efab9ef328bdf4c3c78c1589b4590d
GiSeok Kang <ime.rokz@gmail.com>
2024-12-27 19:54:49 +0900

    refactor: DTO 이메일 인증코드 이름 간소화

--- src/main/java/com/solta/email/dto/AuthCodeEmailDTO.java
+++ src/main/java/com/solta/email/dto/AuthCodeEmailDTO.java
@@ -2,9 +2,9 @@ package com.solta.email.dto;
 
 import jakarta.validation.constraints.Pattern;
 
-public record VerificationEmailDTO(
+public record AuthCodeEmailDTO(
         @Pattern(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", message = "올바른 이메일 형식이 아닙니다.")
         String email,
-        String verificationCode
+        String authCode
 ) {
 }
