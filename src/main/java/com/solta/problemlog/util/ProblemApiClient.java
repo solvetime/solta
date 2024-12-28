418e3828d97d464e3b3d18f43ac7dab95819478f
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2024-12-29 01:19:00 +0900

    feat : 문제 정보 요청 api 클라이언트 추가

--- src/main/java/com/solta/problemlog/util/ProblemApiClient.java
+++ src/main/java/com/solta/problemlog/util/ProblemApiClient.java
@@ -1,6 +1,11 @@
 package com.solta.problemlog.util;
 
+import com.solta.problemlog.dto.response.ProblemResponseDTO;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,5 +18,23 @@ public class ProblemApiClient {
 
     public ProblemApiClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    public ProblemResponseDTO fetchProblemById(int problemId){
+        String url = String.format("%s/problem/show?problemId=%d", apiUrl, problemId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Accept", "application/json");
+        headers.set("x-solvedac-language", "ko");
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<ProblemResponseDTO> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                ProblemResponseDTO.class
+        );
+
+         return response.getBody();
     }
 }
