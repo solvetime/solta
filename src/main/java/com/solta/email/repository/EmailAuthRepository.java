49a1643b024a344dc0392b4bb52d08827144a7eb
GiSeok Kang <ime.rokz@gmail.com>
2024-12-28 18:12:21 +0900

    fix: @Repository 어노테이션 추가

--- src/main/java/com/solta/email/repository/EmailAuthRepository.java
+++ src/main/java/com/solta/email/repository/EmailAuthRepository.java
@@ -4,7 +4,9 @@ import com.solta.email.entity.EmailAuth;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmailAuthRepository extends JpaRepository<EmailAuth, Long> {
 
     Optional<EmailAuth> findByEmail(String email);
