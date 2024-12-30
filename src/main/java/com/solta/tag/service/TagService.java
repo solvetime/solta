a571d0413e3dd050050d6efcc5a019e301e5ade6
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2024-12-30 22:25:45 +0900

    feat : Profile이 local로 설정 되어있을 때만 Tag 테이블을 자동 초기화하도록 수정

--- src/main/java/com/solta/tag/service/TagService.java
+++ src/main/java/com/solta/tag/service/TagService.java
@@ -5,9 +5,14 @@ import com.solta.tag.dto.TagDisplayName;
 import com.solta.tag.dto.TagItem;
 import com.solta.tag.dto.response.TagResponse;
 import com.solta.tag.repository.TagRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -19,24 +24,28 @@ public class TagService {
     private final RestTemplate restTemplate;
     private final TagRepository tagRepository;
     private String apiUrl = "https://solved.ac/api/v3/tag/list";
+    private final Environment env;
 
-    public TagService(RestTemplate restTemplate, TagRepository tagRepository) {
+    public TagService(RestTemplate restTemplate, TagRepository tagRepository, Environment env) {
         this.restTemplate = restTemplate;
         this.tagRepository = tagRepository;
+        this.env = env;
     }
 
     @EventListener(ApplicationReadyEvent.class)
     @Transactional
     public void updateTags(){
-        TagResponse tagResponse = fetchTagsFromApi();
+        if (Arrays.asList(env.getActiveProfiles()).contains("local")) {
+            TagResponse tagResponse = fetchTagsFromApi();
 
-        for(TagItem tagItem : tagResponse.getItems()){
-            String tagKey = tagItem.getKey();
-            String nameKo = extractDisplayName(tagItem, "ko");
+            for(TagItem tagItem : tagResponse.getItems()){
+                String tagKey = tagItem.getKey();
+                String nameKo = extractDisplayName(tagItem, "ko");
 
-            Tag tag = new Tag(tagKey, nameKo);
+                Tag tag = new Tag(tagKey, nameKo);
 
-            tagRepository.save(tag);
+                tagRepository.save(tag);
+            }
         }
     }
 
