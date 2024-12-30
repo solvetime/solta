c504ec0e065cf599faded06957bb3093370065de
GiSeok Kang <ime.rokz@gmail.com>
2024-12-31 00:15:21 +0900

    feat: 유저 정보 가져오는 커스텀 어노테이션 추가

--- src/main/java/com/solta/global/config/WebMvcConfig.java
+++ src/main/java/com/solta/global/config/WebMvcConfig.java
@@ -1,8 +1,11 @@
 package com.solta.global.config;
 
 import com.solta.auth.AuthInterceptor;
+import com.solta.global.token.LoginUserArgumentResolver;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -19,5 +23,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/login")
                 .excludePathPatterns("/member/signup")
                 .excludePathPatterns("/email/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(loginUserArgumentResolver);
     }
 }
---
+++ src/main/java/com/solta/global/token/LoginUserArgumentResolver.java
@@ -0,0 +1,33 @@
+package com.solta.global.token;
+
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenManager jwtTokenManager;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(CurrentUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        String token = webRequest.getHeader("Authorization");
+        if (token == null) {
+            throw new NoSuchElementException();
+        }
+
+        return jwtTokenManager.getParsedClaims(token);
+    }
+}
