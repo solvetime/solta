3c8de9abb63b3029a34f621e9ec6bf7eff1ef202
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2024-12-30 16:33:09 +0900

    chore : tier 테이블 초기화를 위한 data.sql 추가

---
+++ src/main/resources/data-local.sql
@@ -0,0 +1,43 @@
+use solta;
+
+-- Bronze
+INSERT INTO tier (name) VALUES ('Bronze V');
+INSERT INTO tier (name) VALUES ('Bronze IV');
+INSERT INTO tier (name) VALUES ('Bronze III');
+INSERT INTO tier (name) VALUES ('Bronze II');
+INSERT INTO tier (name) VALUES ('Bronze I');
+
+-- Silver
+INSERT INTO tier (name) VALUES ('Silver V');
+INSERT INTO tier (name) VALUES ('Silver IV');
+INSERT INTO tier (name) VALUES ('Silver III');
+INSERT INTO tier (name) VALUES ('Silver II');
+INSERT INTO tier (name) VALUES ('Silver I');
+
+-- Gold
+INSERT INTO tier (name) VALUES ('Gold V');
+INSERT INTO tier (name) VALUES ('Gold IV');
+INSERT INTO tier (name) VALUES ('Gold III');
+INSERT INTO tier (name) VALUES ('Gold II');
+INSERT INTO tier (name) VALUES ('Gold I');
+
+-- Platinum
+INSERT INTO tier (name) VALUES ('Platinum V');
+INSERT INTO tier (name) VALUES ('Platinum IV');
+INSERT INTO tier (name) VALUES ('Platinum III');
+INSERT INTO tier (name) VALUES ('Platinum II');
+INSERT INTO tier (name) VALUES ('Platinum I');
+
+-- Diamond
+INSERT INTO tier (name) VALUES ('Diamond V');
+INSERT INTO tier (name) VALUES ('Diamond IV');
+INSERT INTO tier (name) VALUES ('Diamond III');
+INSERT INTO tier (name) VALUES ('Diamond II');
+INSERT INTO tier (name) VALUES ('Diamond I');
+
+-- Ruby
+INSERT INTO tier (name) VALUES ('Ruby V');
+INSERT INTO tier (name) VALUES ('Ruby IV');
+INSERT INTO tier (name) VALUES ('Ruby III');
+INSERT INTO tier (name) VALUES ('Ruby II');
+INSERT INTO tier (name) VALUES ('Ruby I');
