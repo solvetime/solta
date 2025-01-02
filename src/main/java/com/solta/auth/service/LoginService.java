bea32b5a83a83d87c8cefd8a80622e8088722417
GiSeok Kang <ime.rokz@gmail.com>
2025-01-02 19:52:41 +0900

    refactor: MemberRepository를 JPA 인터페이스에 의존하도록 변경
    - 기존에 의미없이 두 개의 Repository를 사용함

--- src/main/java/com/solta/auth/service/LoginService.java
+++ src/main/java/com/solta/auth/service/LoginService.java
@@ -3,6 +3,7 @@ package com.solta.auth.service;
 import com.solta.auth.dto.AuthInfo;
 import com.solta.auth.dto.LoginRequest;
 import com.solta.member.domain.Member;
+import com.solta.member.exception.NoMemberExistsException;
 import com.solta.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,9 @@ public class LoginService {
     private final MemberRepository memberRepository;
 
     public AuthInfo login(LoginRequest loginRequest) {
-        Member member = memberRepository.findByEmail(loginRequest.email());
+        Member member = memberRepository.findByEmail(loginRequest.email())
+                .orElseThrow(NoMemberExistsException::new);
+
         return new AuthInfo(member.getId(), member.getEmail(), member.getName());
     }
 }
