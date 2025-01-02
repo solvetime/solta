1b04ae396e6bffb4b2d47911227f203e6e529056
GiSeok Kang <ime.rokz@gmail.com>
2025-01-02 19:53:30 +0900

    refactor: 공통 API 반환하도록 코드 수정

--- src/main/java/com/solta/email/controller/EmailController.java
+++ src/main/java/com/solta/email/controller/EmailController.java
@@ -3,12 +3,17 @@ package com.solta.email.controller;
 
 import com.solta.email.dto.request.EmailDTO;
 import com.solta.email.dto.request.AuthCodeEmailDTO;
+import com.solta.email.dto.response.EmailWithAuthCodeDTO;
 import com.solta.email.service.EmailService;
+import com.solta.global.common.response.ApiResponse;
+import com.solta.global.common.response.HttpMessage;
 import com.solta.member.domain.Member;
 import com.solta.member.repository.MemberRepository;
 import jakarta.validation.Valid;
-import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,26 +29,48 @@ public class EmailController {
     private final MemberRepository memberRepository;
 
     @PostMapping("/exists")
-    public ResponseEntity<EmailDTO> existsEmail(@RequestBody @Valid EmailDTO email) {
-        try {
-            Member byEmail = memberRepository.findByEmail(email.email());
-            return ResponseEntity.ok(email);
-        } catch (NoSuchElementException e) {
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity<ApiResponse<EmailDTO>> existsEmail(@Valid @RequestBody EmailDTO email) {
+        Optional<Member> byEmail = memberRepository.findByEmail(email.email());
+
+        if (byEmail.isEmpty()) {
+            return ApiResponse.<EmailDTO>builder()
+                    .message(HttpMessage.ERROR.getMsg())
+                    .data(email)
+                    .build()
+                    .toEntity(HttpStatus.BAD_REQUEST);
         }
+
+        return ApiResponse.<EmailDTO>builder()
+                .message(HttpMessage.SUCCESS.getMsg())
+                .data(email)
+                .build()
+                .toEntity(HttpStatus.OK);
     }
 
     @PostMapping("/send")
-    public ResponseEntity<Void> sendEmail(@RequestBody @Valid EmailDTO email) {
-        emailService.sendEmail(email);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ApiResponse<EmailWithAuthCodeDTO>> sendEmail(@Valid @RequestBody EmailDTO email) {
+        EmailWithAuthCodeDTO result = emailService.sendEmail(email).join();// 이거 해결 ㄱ
+
+        return ApiResponse.<EmailWithAuthCodeDTO>builder()
+                .message(HttpMessage.SUCCESS.getMsg())
+                .data(result)
+                .build()
+                .toEntity(HttpStatus.OK);
     }
 
     @PostMapping("/verify")
-    public ResponseEntity<Void> verifyEmail(@Valid @RequestBody AuthCodeEmailDTO authCodeEmailDTO) {
+    public ResponseEntity<ApiResponse<EmailWithAuthCodeDTO>> verifyEmail(@Valid @RequestBody AuthCodeEmailDTO authCodeEmailDTO) {
         if (emailService.verifyEmail(authCodeEmailDTO.email(), authCodeEmailDTO.authCode())) {
-            return ResponseEntity.ok().build();
+            return ApiResponse.<EmailWithAuthCodeDTO>builder()
+                    .message(HttpMessage.SUCCESS.getMsg())
+                    .data(new EmailWithAuthCodeDTO(authCodeEmailDTO.email(), authCodeEmailDTO.authCode()))
+                    .build()
+                    .toEntity(HttpStatus.OK);
         }
-        return ResponseEntity.badRequest().build();
+        return ApiResponse.<EmailWithAuthCodeDTO>builder()
+                .message(HttpMessage.ERROR.getMsg())
+                .data(new EmailWithAuthCodeDTO(authCodeEmailDTO.email(), authCodeEmailDTO.authCode()))
+                .build()
+                .toEntity(HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/solta/member/controller/MemberController.java
+++ src/main/java/com/solta/member/controller/MemberController.java
@@ -1,10 +1,13 @@
 package com.solta.member.controller;
 
 import com.solta.email.service.EmailService;
+import com.solta.global.common.response.ApiResponse;
+import com.solta.global.common.response.HttpMessage;
 import com.solta.member.domain.Member;
 import com.solta.member.dto.response.SignUpResponse;
 import com.solta.member.service.MemberService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +25,20 @@ public class MemberController {
     private final EmailService emailService;
 
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponse> signup(@RequestBody SignUpDTO signUpDTO) {
+    public ResponseEntity<ApiResponse<SignUpResponse>> signup(@RequestBody SignUpDTO signUpDTO) {
         if (emailService.verifyEmail(signUpDTO.email(), signUpDTO.authCode()) &&
                 !memberService.isExistEmail(signUpDTO.email())) {
             Member member = memberService.signUp(signUpDTO);
-            return ResponseEntity.ok(SignUpResponse.from(member));
+            return ApiResponse.<SignUpResponse>builder()
+                    .message(HttpMessage.SUCCESS.getMsg())
+                    .data(SignUpResponse.from(member))
+                    .build()
+                    .toEntity(HttpStatus.OK);
         }
 
-        return ResponseEntity.badRequest().build();
+        return ApiResponse.<SignUpResponse>builder()
+                .message(HttpMessage.ERROR.getMsg())
+                .build()
+                .toEntity(HttpStatus.BAD_REQUEST);
     }
 }
