4cca24baf76ff6d03b813381cf40e39e508dbae1
JaeIk Choi <107412414+choijake@users.noreply.github.com>
2025-01-11 19:29:18 +0900

    feat : 기록 등록 요청 값에 대한 검증 추가 #13

--- src/main/java/com/solta/problemlog/dto/request/ProblemLogRequestDTO.java
+++ src/main/java/com/solta/problemlog/dto/request/ProblemLogRequestDTO.java
@@ -1,11 +1,18 @@
 package com.solta.problemlog.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 public class ProblemLogRequestDTO {
+    @NotNull(message = "문제 번호는 필수 입력값입니다.")
     private Integer problemNumber;
-    private Integer solveDuration;
+    @NotEmpty(message = "풀이 시간은 필수 입력값입니다.")
+    @Pattern(
+            regexp = "^\\d{1,2}:\\d{2}:\\d{2}$",
+            message = "해결 시간은 HH:mm:ss 형식이어야 합니다."
+    )
+    private String solveDuration;
 }
